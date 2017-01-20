3b060b0508866968d27356f4c2aa4eae8b228e59
Marcela Silva <marcelabsr@me.com>
2017-01-20 14:31:53 -0500

    video still broken

--- public/script.js
+++ public/script.js
@@ -5,23 +5,22 @@ var previewMedia;
 var identity;
 var roomName;
 
-// Check for WebRTC
+
 if (!navigator.webkitGetUserMedia && !navigator.mozGetUserMedia) {
   alert('WebRTC is not available in your browser.');
 }
 
-// When we are about to transition away from this page, disconnect
-// from the room, if joined.
+
 window.addEventListener('beforeunload', leaveRoomIfJoined);
 
 $.getJSON('/token', function (data) {
   identity = data.identity;
 
-  // Create a Video Client and connect to Twilio
+
   videoClient = new Twilio.Video.Client(data.token);
   document.getElementById('room-controls').style.display = 'block';
 
-  // Bind button to join room
+
   document.getElementById('button-join').onclick = function () {
     roomName = document.getElementById('room-name').value;
     if (roomName) {
@@ -36,14 +35,14 @@ $.getJSON('/token', function (data) {
     }
   };
 
-  // Bind button to leave room
+
   document.getElementById('button-leave').onclick = function () {
     log('Leaving room...');
     activeRoom.disconnect();
   };
 });
 
-// Successfully connected!
+
 function roomJoined(room) {
   activeRoom = room;
 
@@ -51,7 +50,7 @@ function roomJoined(room) {
   document.getElementById('button-join').style.display = 'none';
   document.getElementById('button-leave').style.display = 'inline';
 
-  // Draw local video, if not already previewing
+
   if (!previewMedia) {
     room.localParticipant.media.attach('#local-media');
   }
@@ -61,20 +60,19 @@ function roomJoined(room) {
     participant.media.attach('#remote-media');
   });
 
-  // When a participant joins, draw their video on screen
+
   room.on('participantConnected', function (participant) {
     log("Joining: '" + participant.identity + "'");
     participant.media.attach('#remote-media');
   });
 
-  // When a participant disconnects, note in log
+
   room.on('participantDisconnected', function (participant) {
     log("Participant '" + participant.identity + "' left the room");
     participant.media.detach();
   });
 
-  // When we are disconnected, stop capturing local video
-  // Also remove media for all remote participants
+
   room.on('disconnected', function () {
     log('Left');
     room.localParticipant.media.detach();
@@ -87,7 +85,7 @@ function roomJoined(room) {
   });
 }
 
-//  Local video preview
+
 document.getElementById('button-preview').onclick = function () {
   if (!previewMedia) {
     previewMedia = new Twilio.Video.LocalMedia();
@@ -103,7 +101,7 @@ document.getElementById('button-preview').onclick = function () {
   };
 };
 
-// Activity log
+
 function log(message) {
   var logDiv = document.getElementById('log');
   logDiv.innerHTML += '<p>&gt;&nbsp;' + message + '</p>';
@@ -123,28 +121,28 @@ const client = new Video.Client(accessToken);
 
 const localMedia = new Video.LocalMedia();
 
-// You can call getUserMedia and add the resulting MediaStream to localMedia.
+
 Video.getUserMedia().then(mediaStream => {
   localMedia.addStream(mediaStream);
 });
 
-// Or you can add the microphone and camera independently.
+
 localMedia.addMicrophone().then(() => {
   return localMedia.addCamera();
 });
 
-// Remove the microphone.
+
 localMedia.removeMicrophone();
 
-// Remove the camera.
+
 localMedia.removeCamera();
 
-// Remove any tracks on the localMedia.
+
 localMedia.tracks.forEach(track => {
   localMedia.removeTrack(track);
 });
 
-// Stop localMedia and all Tracks on it.
+
 localMedia.stop();
 
 client.connect({
@@ -156,21 +154,21 @@ client.connect({
   console.error('Failed to connect to the Room', error);
 });
 
-// Log your Client's LocalParticipant in the Room
+
 const localParticipant = room.localParticipant;
 console.log('Connected to the Room as LocalParticipant "%s"', localParticipant.identity);
 
-// Log any Participants already connected to the Room
+
 room.participants.forEach(participant => {
   console.log('Participant "%s" is connected to the Room', participant.identity);
 });
 
-// Log new Participants as they connect to the Room
+
 room.once('participantConnected', participant => {
   console.log('Participant "%s" has connected to the Room', participant.identity);
 });
 
-// Log Participants as they disconnect from the Room
+
 room.once('participantDisconnected', participant => {
   console.log('Participant "%s" has disconnected from Room', participant.identity);
 });
@@ -207,39 +205,29 @@ participant.on('trackDisabled', track => {
   }
 });
 
-// You can attach a Participant's Media directly to a DOM element,
+
 participant.media.attach(document.getElementById('#my-view'));
 
-// You can pass the query selector directly,
+
 participant.media.attach('#media-view');
 
-// Or you can create a default element.
+
 const element = participant.media.attach();
 document.body.appendChild(element);
 
-// In all three of these scenarios, as Tracks are added and removed, the
-// attached element will be updated with the appropriate <audio> and <video>
-// tags. If you would like to manage Track attachment yourself, you can always
-// attach them manually. For example,
+
 participant.media.tracks.forEach(track => {
   track.attach('#track-view');
 });
 
 
 
-// We use jQuery to make an Ajax request to the server to retrieve our
-// Access Token
+
 $.getJSON('/token', function(data) {
-    // The data sent back from the server should contain a long string - this is
-    // the JWT token you need to initialize the SDK. Read more about JWT
-    // (JSON Web Token) at http://jwt.io
+
     console.log(data.token);
 
-    // Since the quickstart app doesn't implement authentication, the server
-    // sends back a randomly generated username for the current client, which is
-    // how they will be identified when sending and receiving Conversation
-    // invites. If your app has an existing authentication system, you can use
-    // the e-mail address or username that uniquely identifies a user instead.
+
     console.log(data.identity);
 });
 
--- token.js
+++ token.js
@@ -1,25 +1,25 @@
 var AccessToken = require('twilio').AccessToken;
 
-// Substitute your Twilio AccountSid and ApiKey details
+
 var ACCOUNT_SID = process.env.ACCOUNT_SID;
 var API_KEY_SID = process.env.API_KEY_SID;
 var API_KEY_SECRET = process.env.API_KEY_SECRET;
 
-// Create an Access Token
+
 var accessToken = new AccessToken(
   ACCOUNT_SID,
   API_KEY_SID,
   API_KEY_SECRET
 );
 
-// Set the Identity of this token
+
 accessToken.identity = 'Marcela';
 
-// Grant access to Conversations
+
 var grant = new AccessToken.ConversationsGrant();
 grant.configurationProfileSid = 'configurationProfileSid';
 accessToken.addGrant(grant);
 
-// Serialize the token as a JWT
+
 var jwt = accessToken.toJwt();
 console.log(jwt);
